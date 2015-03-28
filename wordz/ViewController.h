56b6aaa929117746c4013d0cacb982804ce2949d
Robin Malhotra <malhotra.robin@yahoo.co.in>
2015-03-28 14:41:57 +0530

    added first table view

--- wordz/ViewController.h
+++ wordz/ViewController.h
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic,strong)UITableView *initialTable;
 
 @end
 
--- wordz/ViewController.m
+++ wordz/ViewController.m
@@ -16,8 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.initialTable=[[UITableView alloc]initWithFrame:self.view.frame];
+    [self.initialTable setDataSource:self];
+    [self.initialTable setDelegate:self];
+    [self.view addSubview:self.initialTable];
+    [self.initialTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
     [self.view setBackgroundColor:[UIColor orangeColor]];
+    [self.initialTable setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +31,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 200;
+    
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell"];
+    UIImageView *image=[[UIImageView alloc]init];
+    image.frame=CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 200);
+    [image setImage:[UIImage imageNamed:@"debate"]];
+    [cell addSubview:image];
+    
+    switch (indexPath.row)
+    {
+        case 0:
+            cell.backgroundColor=[UIColor colorWithRed:222.0/256 green:226.0/256 blue:214.0/256 alpha:1];
+            break;
+            
+        default:
+            break;
+    }
+    return cell;
+}
 @end
+
