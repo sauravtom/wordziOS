35a352472618798256f10808bbebfda6f7f72ea3
sauravtom <stomatrix@gmail.com>
2015-03-28 14:59:14 +0530

    added more images in first table view

--- wordz/ViewController.m
+++ wordz/ViewController.m
@@ -23,6 +23,7 @@
     [self.initialTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
     [self.view setBackgroundColor:[UIColor orangeColor]];
     [self.initialTable setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    [self.initialTable setAllowsSelection:NO];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -51,15 +52,26 @@
     UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell"];
     UIImageView *image=[[UIImageView alloc]init];
     image.frame=CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 200);
-    [image setImage:[UIImage imageNamed:@"debate"]];
-    [cell addSubview:image];
+    [image setContentMode:UIViewContentModeScaleAspectFit];
     
     switch (indexPath.row)
     {
         case 0:
             cell.backgroundColor=[UIColor colorWithRed:222.0/256 green:226.0/256 blue:214.0/256 alpha:1];
+            [image setImage:[UIImage imageNamed:@"debate"]];
+            [cell addSubview:image];
             break;
-            
+        case 1:
+            cell.backgroundColor=[UIColor colorWithRed:202.0/256 green:207.0/256 blue:214.0/256 alpha:1];
+            [image setImage:[UIImage imageNamed:@"clock_dark"]];
+            [cell addSubview:image];
+            break;
+        case 2:
+            cell.backgroundColor=[UIColor colorWithRed:222.0/256 green:227.0/256 blue:114.0/256 alpha:1];
+            [image setImage:[UIImage imageNamed:@"ic_support"]];
+            [cell addSubview:image];
+            break;
+    
         default:
             break;
     }
